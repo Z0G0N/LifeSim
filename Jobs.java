28fbb4c5bddfa9955dc34de63f13aa35de7a74cc
Z0G0N <caydenchandler@gmail.com>
2023-05-06 00:56:47 -0400

    job update

---
+++ Jobs.java
@@ -0,0 +1,83 @@
+class Jobs {
+
+
+
+    public int Delivery(Player player) {
+        //random money system
+        //Delivery: $10-$20 per hour, driver's license, reliable vehicle, navigation skills
+        player.skills.setHungerfordSkill(100);
+        return 1;
+    }
+
+    public static int Uber(Player player) {
+        //random money system
+        //Uber: $15-$25 per hour, driver's license, reliable vehicle, navigation skills, good driving record, customer service skills
+        return 1;
+    }
+
+    public static int Pizza_worker(Player player) {
+        //random money system
+        //Pizza_worker: $8-$12 per hour plus tips, basic math skills, good customer service skills, ability to work in a fast-paced environment
+        return 1;
+    }
+
+    public static int Fast_Food_Worker(Player player) {
+        //random money system
+        //Fast_Food_Worker: $8-$12 per hour plus tips, basic math skills, good customer service skills, ability to work in a fast-paced environment
+        return 1;
+    }
+
+    public static int Mail_Man(Player player) {
+        //random money system
+        //Mail_Man: $15-$25 per hour, driver's license, ability to lift and carry heavy packages, good time management skills, customer service skills
+        return 1;
+    }
+
+    public static int Teacher(Player player) {
+        //random money system
+        //Teacher: $25-$50 per hour, Bachelor's degree in Education or related field, teaching certificate, excellent communication and organizational skills
+        return 1;
+    }
+    
+    public static int Professor(Player player) {
+        //random money system
+        //Professor: $50-$100 per hour, PhD in related field, teaching experience, excellent communication and organizational skills
+        return 1;
+    }
+
+    public static int Engineer(Player player) {
+        //random money system
+        //Engineer: $60,000-$100,000 per year, Bachelor's degree in Engineering or related field, knowledge of relevant software and programming languages, excellent problem-solving and analytical skills
+        return 1;
+    }
+
+    public static int Developer(Player player) {
+        //random money system
+        //Developer: $60,000-$120,000 per year, Bachelor's degree in Computer Science or related field, knowledge of relevant software and programming languages, excellent problem-solving and analytical skills
+        return 1;
+    }
+    
+    public static int Salesman(Player player) {
+        //random money system
+        //Salesman: $30,000-$80,000 per year plus commission, excellent communication and interpersonal skills, ability to negotiate and close deals, good time management and organizational skills
+        return 1;
+    }
+
+    public static int Lawyer(Player player) {
+        //random money system
+        //Lawyer: $80,000-$150,000 per year, Juris Doctor degree, passed the bar exam, excellent communication and analytical skills, ability to research and interpret laws and regulations
+        return 1;
+    }
+
+    public static int Doctor(Player player) {
+        //random money system
+        //Doctor: $200,000-$500,000 per year, Medical degree, residency and licensure, excellent medical knowledge and diagnostic skills, ability to communicate with patients and collaborate with other medical professionals
+        return 1;
+    }
+
+    public static int Astronaut(Player player) {
+        //random money system
+        //Astronaut: $66,000-$144,000 per year, Bachelor's degree in a STEM field, excellent physical health and fitness, ability to work in confined spaces and under pressure, experience in aviation or space-related fields
+        return 1;
+    }
+}
